00595b3539ad397d5cf953cbd41f180cb6f48ce6
Jean Santos <shadowjean911@gmail.com>
2022-08-31 07:53:46 -0300

    feat: make request to display popular movies in home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+import "./MoviesGrid.css"
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([])
+
+  const getPopularMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setPopularMovies(data.results)
+  };
+
+  useEffect(() => {
+
+    const popularUrl = `${moviesURL}popular?${apiKey}`;
+
+    getPopularMovies(popularUrl);
+  }, [])
+
+  return (
+    <div className="container">
+      <h2 className="title">Filmes mais populares:</h2>
+      <div className="movies-container">
+        {popularMovies.length === 0 && <p>Carregando...</p>}
+        {popularMovies.length > 0 &&
+         popularMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  )
+}
+
+export default Home
